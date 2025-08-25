adc172782e9dddc68b42ea72cd0277a1bedc64d6
Ami <amir1608@gmail.com>
2025-08-25 15:02:44 -0600

    cambios en la base

--- validacion.py
+++ validacion.py
@@ -1,39 +1,29 @@
 #holis esta es mi pequeña parte
 import sqlite3
 
+
+# Funciones de base de datos
 def creartabla():
     baseDeDatos = sqlite3.connect("adivinar.db")
     cr = baseDeDatos.cursor()
-
-    # Creamos la tabla
-    cr.execute('''
+    cr.execute(''' 
         CREATE TABLE IF NOT EXISTS palabras (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             palabra TEXT NOT NULL,
             descripcion TEXT NOT NULL
         )
     ''')
-
-    # Guardamos los cambios y cerramos la conexión
     baseDeDatos.commit()
     baseDeDatos.close()
 
+
 def ingresarpalabra(palabra, descripcion):
-    # Insertamos una nueva palabra
     baseDeDatos = sqlite3.connect("adivinar.db")
     cr = baseDeDatos.cursor()
-    cr.execute('''
-        INSERT INTO palabras (palabra, descripcion)
+    cr.execute(''' 
+        INSERT INTO palabras (palabra, descripcion) 
         VALUES (?, ?)
     ''', (palabra, descripcion))
     baseDeDatos.commit()
     baseDeDatos.close()
 
-def palabrarandom():
-    # Obtenemos una palabra randomm
-    baseDeDatos = sqlite3.connect("adivinar.db")
-    cursor = baseDeDatos.cursor()
-    cursor.execute('SELECT palabra, descripcion FROM palabras ORDER BY RANDOM() LIMIT 1')
-    palabra = cursor.fetchone()
-    baseDeDatos.close()
-    return palabra
