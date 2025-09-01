bad04def711bffe2f6bc1a2af4bc771c08e4bbeb
Estudiante Vespertina <estudiantev@COED.LOCAL>
2025-09-01 15:04:27 -0600

    primera ventana terminada (creo)

--- interfazpro.py
+++ interfazpro.py
@@ -3,20 +3,25 @@ from customtkinter import*
 
 app = CTk()
 app.title ("Adivina la palabra")
-ventana_inicial = CTkFrame(app)
-ventana_inicial.grid(column=0,row=0,padx=3, pady=3)
 
-titulo=CTkLabel(ventana_inicial, text="BIENVENIDO",font=("Arial",20))
-titulo.grid(column=0,row=0, padx=3, pady=3)
-jugar=CTkButton(ventana_inicial, text="generar palabra")
-jugar.grid(column=0,row=1, padx=3, pady=3)
-salir=CTkButton(ventana_inicial, text="agregar palabra")
-salir.grid(column=0,row=2, padx=3, pady=3)
+caja1 =CTkEntry(app,width=150, height=100, font=("Arial",20),justify="right")
+caja2 =CTkEntry(app,width=150, height=40, font=("Arial",20),justify="right")
+
+titulo=CTkLabel(app, text="ADIVINA LA PALABRA",font=("Arial",20))
+titulo.grid(column=0,row=0, padx=3, pady=3, columnspan= 5)
+genp=CTkButton(app, text="Generar palabra")
+genp.grid(column=1,row=1, padx=3, pady=3)
+agp=CTkButton(app, text="Agregar palabra")
+agp.grid(column=0,row=1, padx=3, pady=3)
+verificar=CTkButton(app, text="Verificar")
+verificar.grid(column=0,row=5,columnspan= 5, padx=3, pady=3)
+caja1.grid(column=0, row=2, columnspan= 5, padx=5,pady=10,sticky="WE")
+caja2.grid(column=0, row=3, columnspan= 5, padx=5,pady=10,sticky="WE")
 
 
 
 
 
-app.resizable(width=False, height=False)
+app.resizable(width=True, height=True)
 
 app.mainloop()
